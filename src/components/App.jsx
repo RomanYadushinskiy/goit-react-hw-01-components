df65424925ed47cf699b4dad7b56e1dacd1e2417
rdush <rdush824@gmail.com>
2022-12-10 00:14:10 +0200

    ДЗ до здачі

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
 import user from './JsonData/user.json';
 import data from './JsonData/data.json';
-
+import friends from './JsonData/friends.json';
+import transactions from './JsonData/transactions.json';
 
 export const App = () => {
   return (
@@ -14,6 +18,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats} />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
